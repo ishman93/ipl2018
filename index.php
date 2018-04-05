b08f64ea249b5872e9edb5088ba07cf8b3e560fd
Ishman <Ishman@192.168.1.7>
2018-04-06 04:38:39 +0530

    fd

--- index.php
+++ index.php
@@ -4,7 +4,11 @@ $dbport = getenv("MYSQL_SERVICE_PORT");
 $dbuser = getenv("user");
 $dbpwd = getenv("password");
 $dbname = getenv("ipl2018db");
-
+printf($dbhost);
+printf($dbport);
+printf($dbuser);
+printf($dbpwd);
+printf($dbname);
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if($connection->connect_errno){
    printf("Connection failed :%s\n", $mysqli -> connect_error);
@@ -14,3 +18,18 @@ if($connection->connect_errno){
 }
 $connection->close();
 ?> 
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
